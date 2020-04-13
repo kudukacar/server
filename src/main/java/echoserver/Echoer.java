6d722ba6da1969fb82f5e5fea0d3c2a25e88a419
manju <manju.thomas083@gmail.com>
2020-04-13 07:25:13 -0400

    add welcome message

--- src/main/java/echoserver/Echoer.java
+++ src/main/java/echoserver/Echoer.java
@@ -3,9 +3,16 @@ package echoserver;
 import java.io.IOException;
 
 public class Echoer implements Echoable {
+    private String message;
+
+    public Echoer(String message) {
+        this.message = message;
+    }
+
     public void echo(Connection connection) throws IOException {
         String input;
         try {
+            connection.write(this.message);
             while ((input = connection.read()) != null) {
                 connection.write(input);
             }
--- src/main/java/echoserver/Main.java
+++ src/main/java/echoserver/Main.java
@@ -5,7 +5,8 @@ import java.net.ServerSocket;
 public class Main {
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(5000);
-        Echoer echoer = new Echoer();
+        String welcomeMessage = "Welcome.  Enter a text to echo.  Press control + C to end.";
+        Echoer echoer = new Echoer(welcomeMessage);
 
         try(Listener listener = new Listener(serverSocket);) {
             new EchoServer(listener, echoer).start();
